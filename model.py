986b74e67cb30321d3543310c2d25622cb65109d
Abhay <abhayhmgi@gmail.com>
2024-12-15 13:33:27 +0530

    PositionalEncoding

--- model.py
+++ model.py
@@ -2,11 +2,38 @@ import torch
 import torch.nn as nn
 import math
 
-class InputEmbeddings(nn.Embedding):
-    def __init__(self, d_model: int, vocab_size: int):
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super.__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(d_model, vocab_size)
 
-    def forward(self, vocab):
-        self.embedding(vocab) * math.sqrt(self.d_model)
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super.__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Create a tensor of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arrange(0, seq_len, dtype=torch.float).unsqueze(1)
+        div_term = torch.exp(torch.arrange(0, d_model, 2).float() * (-math.log(1000) / d_model))
+
+        # Apply Sin and Costo even and odd positions respectively
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+
+        # Create register buffer for batch processing
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
+        return self.dropout(x)
