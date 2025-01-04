be25ad7aa7692acd682262c3317304d3915502fd
Abhay <abhayhmgi@gmail.com>
2025-01-04 15:55:00 +0530

    Encoder

--- model.py
+++ model.py
@@ -116,3 +116,15 @@ class ResidualConnection(nn.Module):
 
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.orm(x)))
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.resudial_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.resudial_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.resudial_connections[1](x, self.feed_foreard_block)
+        return x
